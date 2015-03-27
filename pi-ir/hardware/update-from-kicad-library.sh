d5837c103d3841d51085c4fb7558abfb1d9959e1
Joe Ferner <joe@fernsroth.com>
2015-03-27 19:24:40 -0400

    switch to new kicad format

--- pi-ir/hardware/update-from-kicad-library.sh
+++ pi-ir/hardware/update-from-kicad-library.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-kicad-update -i pi-ir.mod.list -o pi-ir.mod --basedir ./kicad-library
-kicad-update -i pi-ir.lib.list -o pi-ir.lib --basedir ./kicad-library
+echo "Sync'ing mods"
+for f in mods/*
+do
+  FILENAME=$(basename $f)
+  KICADLIB_FILENAME=../../../kicad-library/mods/${FILENAME}
+  LOCAL_FILENAME=./mods/${FILENAME}
+  if [ -f ${KICADLIB_FILENAME} ]; then
+    cp ${KICADLIB_FILENAME} ${LOCAL_FILENAME}
+  else
+    echo "Could not find ${KICADLIB_FILENAME}"
+  fi
+done
+
+kicad-update -i pi-ir.lib.list -o pi-ir.lib --basedir ../../../kicad-library
