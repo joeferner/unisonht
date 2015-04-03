e16eab29678b70b40a8b95aae96358e5699b3485
David Singley <dsingley@nearinfinity.com>
2015-04-03 19:41:51 -0400

    copy all config files

--- deploy-to-pi.sh
+++ deploy-to-pi.sh
@@ -6,6 +6,6 @@ echo "Deploying to $DEST"
 mvn package dependency:copy-dependencies
 
 rsync -u `find . | grep jar` $DEST:/opt/unisonht
-scp config/help-input.png config/tivo-roamio-pro-remote.jpg $DEST:/opt/unisonht/config
+scp config/* $DEST:/opt/unisonht/config
 rsync -ur plugins/web/src/main/java/webapp pi@unisonht-pi:/opt/unisonht
 
