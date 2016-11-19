1e19ceaa4f8b97b3f13d4061f36cc9d5a4e34653
Joe Ferner <joe@fernsroth.com>
2016-11-19 16:51:43 -0500

    log to file

--- lib/Log.ts
+++ lib/Log.ts
@@ -5,6 +5,17 @@ import {createLogger as bunyanCreateLogger} from "bunyan";
 export default function createLogger(name: string) {
   return bunyanCreateLogger({
     name: name,
-    level: 'debug'
+    level: 'debug',
+    streams: [
+      {
+        stream: process.stdout
+      },
+      {
+        type: 'rotating-file',
+        path: '/var/log/unisonht/unisonht.log',
+        period: '1h',
+        count: 3
+      }
+    ]
   });
 }
