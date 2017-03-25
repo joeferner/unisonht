d996327ae8c0b21cd9d0d92f8c5793fd5eaa4eea
Joe Ferner <joe@fernsroth.com>
2017-03-25 15:00:31 -0400

    add logstash logging

--- lib/Log.ts
+++ lib/Log.ts
@@ -1,17 +1,28 @@
 import * as Logger from "bunyan";
 
 export function createLogger(name: string): Logger {
-  return Logger.createLogger({
+  let options = {
     name: name,
     level: 'debug',
-    streams: [
-      {
-        stream: process.stdout
-      },
-      {
-        type: 'file',
-        path: '/var/log/unisonht/unisonht.log'
-      }
-    ]
+    streams: []
+  };
+  options.streams.push({
+    stream: process.stdout
   });
+  if (process.env.LOG_FILE) {
+    options.streams.push({
+      type: 'file',
+      path: process.env.LOG_FILE
+    });
+  }
+  if (process.env.LOGSTASH_HOST) {
+    options.streams.push({
+      type: 'raw',
+      stream: require('bunyan-logstash').createStream({
+        host: process.env.LOGSTASH_HOST,
+        port: process.env.LOGSTASH_PORT || 5505
+      })
+    });
+  }
+  return Logger.createLogger(options);
 }
