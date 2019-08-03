4bd29afe0769373097f26c44d9cbd1866ca1da12
Joe Ferner <joe@fernsroth.com>
2019-08-02 21:54:19 -0400

    inital rewrite

--- lib/UnisonHTResponse.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-
-export interface UnisonHTResponse extends express.Response {
-  deviceButtonPress: (deviceName: string, buttonName: string) => void;
-  changeMode: (newMode: string) => void;
-  promiseNoContent: (promise: Promise<any>) => void;
-}
