5d1f229bb9e138d2122bdc3df4bd91dbb5858b26
Joe Ferner <joe@fernsroth.com>
2023-09-09 12:50:03 -0400

    rewrite 2023

--- public/src/generated/models/GetGraphResponseEdge.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-import type { UnisonHTEdgeConfig } from './UnisonHTEdgeConfig';
-
-export type GetGraphResponseEdge = {
-    config: UnisonHTEdgeConfig;
-};
--- src/types/OpenApiProvider.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'tst-reflect';
-import { OpenApi } from './openApi/v3/OpenApi';
-
-export interface OpenApiProvider {
-  get openApiTags(): string[];
-  get apiUrlPrefix(): string;
-  getOpenApiType(): Type | undefined;
-  updateOpenApi(openApi: OpenApi): void;
-}
