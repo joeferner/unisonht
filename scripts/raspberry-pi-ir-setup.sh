d8d23f621a1d13341476648eef85a6c2b0cd5ca8
Joe Ferner <joe@fernsroth.com>
2023-09-14 17:32:25 -0400

    udev rules

--- scripts/raspberry-pi-ir-setup.sh
+++ scripts/raspberry-pi-ir-setup.sh
@@ -21,6 +21,21 @@ else
   apt install -y evtest
 fi
 
+# create /sbin/lirc-allow-write-protocol
+echo "create /sbin/lirc-allow-write-protocol"
+cat << EOF > /sbin/lirc-allow-write-protocol
+#!/bin/bash
+
+chmod a+w /sys/class/rc/rc*/protocols
+EOF
+chmod u+x /sbin/lirc-allow-write-protocol
+
+# create /etc/udev/rules.d/50-lirc-protocol.rules
+echo "/etc/udev/rules.d/50-lirc-protocol.rules"
+cat << EOF > /etc/udev/rules.d/50-lirc-protocol.rules
+SUBSYSTEM=="lirc", GROUP="lirc", RUN+="/sbin/lirc-allow-write-protocol"
+EOF
+
 # Update /boot/config.txt
 echo "update /boot/config.txt"
 sed -i 's|.*gpio-ir,.*|dtoverlay=gpio-ir,gpio_pin='"${IN_PIN}"'|' /boot/config.txt
