d9e31d1a21bcc2ee1084def3e55b69dba134b740
Joe Ferner <joe@fernsroth.com>
2024-05-29 18:18:30 -0400

    build 64

--- src/ioctl.rs
+++ src/ioctl.rs
@@ -3,6 +3,14 @@ use nix::convert_ioctl_res;
 use nix::libc::ioctl;
 use std::os::fd::RawFd;
 
+#[cfg(target_pointer_width = "64")]
+pub fn write_u32(fd: RawFd, cmd: u32, data: u32) -> Result<i32> {
+    let data_bytes = data.to_le_bytes();
+    let ret = convert_ioctl_res!(unsafe { ioctl(fd, cmd as u64, data_bytes.as_ptr()) })?;
+    return Result::Ok(ret);
+}
+
+#[cfg(target_pointer_width = "32")]
 pub fn write_u32(fd: RawFd, cmd: u32, data: u32) -> Result<i32> {
     let data_bytes = data.to_le_bytes();
     let ret = convert_ioctl_res!(unsafe { ioctl(fd, cmd, data_bytes.as_ptr()) })?;
